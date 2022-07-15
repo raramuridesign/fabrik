6ccbdb686006b844e177c7ff310fe47872edf2fb
Henk <40104328+joomlahenk@users.noreply.github.com>
2022-07-15 10:28:39 +0200

    Repaired formlist, Issue #94

--- administrator/components/com_fabrik/models/fields/formlist.php
+++ administrator/components/com_fabrik/models/fields/formlist.php
@@ -16,6 +16,10 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Form\Field\ListField;
 
+require_once JPATH_ADMINISTRATOR . '/components/com_fabrik/helpers/element.php';
+
+//JFormHelper::loadFieldClass('list');
+
 /**
  * Renders a repeating drop down list of forms
  *
@@ -27,21 +31,27 @@ use Joomla\CMS\Form\Field\ListField;
 class JFormFieldFormList extends ListField
 {
 	/**
-	 * Element type
+	 * Element name
 	 *
 	 * @access	protected
 	 * @var		string
 	 */
-	protected $type = 'Formlist';
+	protected $name = 'Formlist';
 
 	/**
-	 * Method to get the field input markup.
+	 * Method to get the field options.
 	 *
-	 * @return	string	The field input markup.
+	 * @return  array	The field option objects.
 	 */
 
-	protected function getInput()
+	protected function getOptions()
 	{
+		$app = Factory::getApplication();
+
+		if ($this->element['package'])
+		{
+			$package = $app->setUserState('com_fabrik.package', $this->element['package']);
+		}
 
 		$db = FabrikWorker::getDbo(true);
 		$query = $db->getQuery(true);
@@ -57,7 +67,8 @@ class JFormFieldFormList extends ListField
 		$db->setQuery($query);
 		$rows = $db->loadObjectList();
 
-		foreach ($rows as $row) {
+		foreach ($rows as &$row)
+		{
 			switch ($row->published)
 			{
 				case '0':
@@ -67,10 +78,59 @@ class JFormFieldFormList extends ListField
 					$row->text .= ' [' . Text::_('JTRASHED') . ']';
 					break;
 			}
-			$this->addOption(htmlspecialchars($row->text), ['value'=>$row->value]);
 		}
 
-		return parent::getInput();
+		$o = new stdClass;
+		$o->value = '';
+		$o->text = '';
+		array_unshift($rows, $o);
 
+		return $rows;
+	}
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return	string	The field input markup.
+	 */
+
+	protected function getInput()
+	{
+		$app = Factory::getApplication();
+		$input = $app->input;
+		$option = $input->get('option');
+
+		if (!in_array($option, array('com_modules', 'com_menus', 'com_advancedmodules')))
+		{
+			$db = FabrikWorker::getDbo(true);
+			$query = $db->getQuery(true);
+			$query->select('form_id')->from('#__fabrik_formgroup')->where('group_id = ' . (int) $this->form->getValue('id'));
+			$db->setQuery($query);
+			$this->value = $db->loadResult();
+			$this->form->setValue('form', null, $this->value);
+		}
+
+		if ((int) $this->form->getValue('id') == 0 || !$this->element['readonlyonedit'])
+		{
+			return parent::getInput();
+		}
+		else
+		{
+			$options = (array) $this->getOptions();
+			$v       = '';
+
+			foreach ($options as $opt)
+			{
+				if ($opt->value == $this->value)
+				{
+					$v = $opt->text;
+				}
+			}
+		}
+
+//		return '<input type="hidden" value="' . $this->value . '" name="' . $this->name . '" />' . '<input type="text" value="' . $v
+//		. '" name="form_justalabel" class="readonly" readonly="true" />';
+		return '<input type="hidden" value="' . $this->value . '" name="' . $this->name . '" />' . '<input type="text" value="' . $v
+		. '" name="form_justalabel" class="form-control" readonly />';
 	}
 }
