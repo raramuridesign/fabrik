ed58fae50b91af0f1feba2afcef1b8bcd5bc09f5
Alex Chartier <achartier@admiralsecure.com>
2022-07-28 15:09:06 -0400

    Add ajax to the data parameters

--- components/com_fabrik/layouts/listactions/fabrik-view-button.php
+++ components/com_fabrik/layouts/listactions/fabrik-view-button.php
@@ -14,6 +14,13 @@ defined('_JEXEC') or die('Restricted access');
 $d = $displayData;
 
 ?>
-<a data-loadmethod="<?php echo $d->loadMethod;?>" class="<?php echo $d->class;?> btn-default" <?php echo $d->detailsAttributes; ?>
-	data-list="<?php echo $d->dataList; ?>" href="<?php echo $d->link; ?>" title="<?php echo $d->viewLabel;?>" target="<?php echo $d->viewLinkTarget; ?>">
-<span class="fa fa-search"></span> <?php echo $d->viewText; ?></a>
+<a data-loadmethod="<?php echo $d->loadMethod;?>" 
+    class="<?php echo $d->class;?> btn-default" <?php echo $d->detailsAttributes; ?>
+	data-list="<?php echo $d->dataList; ?>" 
+    data-isajax="<?php echo $d->isAjax; ?>"
+    data-rowid="<?php echo $d->rowId; ?>"
+    data-iscustom="<?php if ($d->isCustom) echo '1'; else echo '0'; ?>"
+    href="<?php echo $d->link; ?>" 
+    title="<?php echo $d->viewLabel;?>" 
+    target="<?php echo $d->viewLinkTarget; ?>">
+    <span class="fa fa-search"></span> <?php echo $d->viewText; ?></a>
