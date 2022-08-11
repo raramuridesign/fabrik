7c3576242ac2e150e9bd923b52064f599495a5ae
Alex Chartier <achartier@admiralsecure.com>
2022-08-11 08:40:48 -0400

    BS5 classes for checkbox filters.

--- components/com_fabrik/layouts/list/filter/fabrik-filter-checkbox.php
+++ components/com_fabrik/layouts/list/filter/fabrik-filter-checkbox.php
@@ -7,8 +7,9 @@ $inputDataAttribs = array('data-filter-name="' . $d->elementName . '"');
 <div class="fabrikListFilterCheckbox">
 <?php
 echo implode("\n",
-				FabrikHelperHTML::grid($d->values, $d->labels, $d->default, $d->name,
-	'checkbox', false, 1, array('input' => array('fabrik_filter')), false, array(), $inputDataAttribs)
-		);
+				FabrikHelperHTML::grid($d->values, $d->labels, $d->default, $d->name, 'checkbox', false, 1, 
+					['input' => ['fabrik_filter', 'form-check-input']], /* Classes */
+					false, array(), $inputDataAttribs)
+						);
 ?>
 </div>
