695cdb300298776838b0d983ceb60644bc2a43c6
Alex Chartier <achartier@admiralsecure.com>
2022-08-19 13:02:59 -0400

    Place the elements into a table so they align in a row.

--- plugins/fabrik_element/birthday/layouts/fabrik-element-birthday-form.php
+++ plugins/fabrik_element/birthday/layouts/fabrik-element-birthday-form.php
@@ -7,9 +7,21 @@ $d = $displayData;
 ?>
 
 <div class="fabrikSubElementContainer" id="<?php echo $d->id;?>">
-	<?php echo HTMLHelper::_('select.genericlist', $d->day_options, $d->day_name, $d->attribs, 'value', 'text', $d->day_value, $d->day_id); ?>
-	<?php echo $d->separator;?>
-	<?php echo HTMLHelper::_('select.genericlist', $d->month_options, $d->month_name, $d->attribs, 'value', 'text', $d->month_value, $d->month_id); ?>
-	<?php echo $d->separator;?>
-	<?php echo HTMLHelper::_('select.genericlist', $d->year_options, $d->year_name, $d->attribs, 'value', 'text', $d->year_value, $d->year_id); ?>
+	<table>
+		<tbody>
+			<tr>
+				<td style="width:130px;">
+					<?php echo HTMLHelper::_('select.genericlist', $d->day_options, $d->day_name, $d->attribs, 'value', 'text', $d->day_value, $d->day_id);?>
+				</td>
+				<td> <?php echo "&nbsp;".$d->separator."&nbsp;";?></td>
+				<td style="width:130px;">
+					<?php echo HTMLHelper::_('select.genericlist', $d->month_options, $d->month_name, $d->attribs, 'value', 'text', $d->month_value, $d->month_id); ?>
+				</td>
+				<td> <?php echo "&nbsp;".$d->separator."&nbsp;";?></td>
+				<td style="width:130px;">
+			<?php echo HTMLHelper::_('select.genericlist', $d->year_options, $d->year_name, $d->attribs, 'value', 'text', $d->year_value, $d->year_id); ?>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
