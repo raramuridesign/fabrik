62922b47e79e33e10f4b472d0114be5882a102e7
Henk <vanklinkenhenk@gmail.com>
2022-08-21 17:25:31 +0200

    Plugins update

--- plugins/fabrik_element/birthday/layouts/fabrik-element-birthday-form.php
+++ plugins/fabrik_element/birthday/layouts/fabrik-element-birthday-form.php
@@ -10,15 +10,15 @@ $d = $displayData;
 	<table>
 		<tbody>
 			<tr>
-				<td style="width:130px;">
+				<td>
 					<?php echo HTMLHelper::_('select.genericlist', $d->day_options, $d->day_name, $d->attribs, 'value', 'text', $d->day_value, $d->day_id);?>
 				</td>
 				<td> <?php echo "&nbsp;".$d->separator."&nbsp;";?></td>
-				<td style="width:130px;">
+				<td>
 					<?php echo HTMLHelper::_('select.genericlist', $d->month_options, $d->month_name, $d->attribs, 'value', 'text', $d->month_value, $d->month_id); ?>
 				</td>
 				<td> <?php echo "&nbsp;".$d->separator."&nbsp;";?></td>
-				<td style="width:130px;">
+				<td>
 			<?php echo HTMLHelper::_('select.genericlist', $d->year_options, $d->year_name, $d->attribs, 'value', 'text', $d->year_value, $d->year_id); ?>
 				</td>
 			</tr>
